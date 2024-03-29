0cf8336ce92e66e7443f700657238307226b45a4
Oscar <oscarrpeulicke@gmail.com>
2024-03-29 13:40:31 +0100

    vec2 clone

--- src/qwick/vec2.ts
+++ src/qwick/vec2.ts
@@ -2,6 +2,8 @@ import { Grid } from "./grid";
 
 export type Vec2 = [number, number];
 
+export const clone = (v: Vec2): Vec2 => [...v];
+
 export const equals = (a: Vec2, b: Vec2): boolean => a[0] === b[0] && a[1] === b[1];
 
 export const add = (a: Vec2, b: Vec2): Vec2 => [a[0] + b[0], a[1] + b[1]];
