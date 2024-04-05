8e1b8c149db1a32700d007a8f5953d194f152e12
Oscar <oscarrpeulicke@gmail.com>
2024-04-06 00:21:10 +0200

    fix

--- src/qwick/event.ts
+++ src/qwick/event.ts
@@ -11,10 +11,16 @@ export type Event = {
     levelNum: number;
 };
 
+const isEvent = (event: unknown): event is Event =>
+    typeof event === "object" && event !== null && "type" in event && "levelNum" in event;
+
 export const emit = (event: Event) => window.parent.postMessage(JSON.stringify(event), "*");
 
 export const subscribe = (func: (event: Event) => void) => {
-    const onMessage = (messageEvent: MessageEvent) => func(JSON.parse(messageEvent.data));
+    const onMessage = (messageEvent: MessageEvent) => {
+        const data = JSON.parse(messageEvent.data);
+        if (isEvent(data)) func(data);
+    };
     window.addEventListener("message", onMessage);
     return () => window.removeEventListener("message", onMessage);
 };
