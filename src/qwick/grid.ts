388574a51553de3b4043ce79c9d1eb92abe85cc8
Oscar <oscarrpeulicke@gmail.com>
2024-04-06 21:53:21 +0200

    grid getCell

--- src/qwick/grid.ts
+++ src/qwick/grid.ts
@@ -8,8 +8,9 @@ export const create = <T>(size: vec2.Vec2, value: T): Grid<T> =>
 export const map = <T1, T2>(grid: Grid<T1>, func: (value: T1, pos: vec2.Vec2) => T2): Grid<T2> =>
     grid.map((row, i) => row.map((value, j) => func(value, [i, j])));
 
-export const getNearestCell = <T>(grid: Grid<T>, pos: vec2.Vec2): T | undefined =>
-    (grid[Math.round(pos[0])] ?? [])[Math.round(pos[1])];
+export const getCell = <T>(grid: Grid<T>, pos: vec2.Vec2): T | undefined => (grid[pos[0]] ?? [])[pos[1]];
+
+export const getNearestCell = <T>(grid: Grid<T>, pos: vec2.Vec2): T | undefined => getCell(grid, vec2.round(pos));
 
 export const transpose = <T>(grid: Grid<T>) => grid[0].map((_, i) => grid.map((_, j) => grid[j][i]));
 
