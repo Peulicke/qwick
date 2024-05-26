2f4b9211925ba10a134f809e86603bd6237689b2
Oscar <oscarrpeulicke@gmail.com>
2024-05-26 13:41:02 +0200

    clean

--- src/qwick/vec2.spec.ts
+++ src/qwick/vec2.spec.ts
@@ -1,5 +1,4 @@
-import { vec2 } from ".";
-import { Vec2, dist, mean, rotate, sum } from "./vec2";
+import * as vec2 from "./vec2";
 
 const epsilon = 1e-10;
 
@@ -37,14 +36,14 @@ describe("unique", () => {
 
 describe("sum", () => {
     it("sums up 0 elements", () => {
-        expect(sum([])).toStrictEqual([0, 0]);
+        expect(vec2.sum([])).toStrictEqual([0, 0]);
     });
     it("sums up 1 element", () => {
-        expect(sum([[1, 2]])).toStrictEqual([1, 2]);
+        expect(vec2.sum([[1, 2]])).toStrictEqual([1, 2]);
     });
     it("sums up 3 elements", () => {
         expect(
-            sum([
+            vec2.sum([
                 [1, 2],
                 [3, 4],
                 [5, 6]
@@ -55,11 +54,11 @@ describe("sum", () => {
 
 describe("mean", () => {
     it("calculates the mean of 1 element", () => {
-        expect(mean([[1, 2]])).toStrictEqual([1, 2]);
+        expect(vec2.mean([[1, 2]])).toStrictEqual([1, 2]);
     });
     it("calculates the mean of 3 elements", () => {
         expect(
-            mean([
+            vec2.mean([
                 [1, 2],
                 [3, 4],
                 [5, 6]
@@ -70,13 +69,13 @@ describe("mean", () => {
 
 describe("rotation", () => {
     it("rotates 0", () => {
-        expect(rotate([1, 1], 0)).toStrictEqual([1, 1]);
+        expect(vec2.rotate([1, 1], 0)).toStrictEqual([1, 1]);
     });
     it("rotates pi/2", () => {
-        const v: Vec2 = [1, 1];
-        const rotated = rotate(v, Math.PI / 2);
-        const expected: Vec2 = [-1, 1];
-        const error = dist(rotated, expected);
+        const v: vec2.Vec2 = [1, 1];
+        const rotated = vec2.rotate(v, Math.PI / 2);
+        const expected: vec2.Vec2 = [-1, 1];
+        const error = vec2.dist(rotated, expected);
         expect(error).toBeLessThan(epsilon);
     });
 });
