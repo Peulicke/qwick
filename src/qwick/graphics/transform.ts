f5644faf2203a38be1eb633cafef00e836c27d1e
Oscar <oscarrpeulicke@gmail.com>
2025-05-04 14:05:39 +0200

    scale2

--- src/qwick/graphics/transform.ts
+++ src/qwick/graphics/transform.ts
@@ -26,6 +26,10 @@ export const scale = (ctx: CanvasRenderingContext2D, s: number): void => {
     ctx.scale(s, s);
 };
 
+export const scale2 = (ctx: CanvasRenderingContext2D, s: vec2.Vec2): void => {
+    ctx.scale(s[0], s[1]);
+};
+
 export const transform = (ctx: CanvasRenderingContext2D, t: Transform2): void => {
     if (t.type === TransformType.Translate) translate(ctx, t.value);
     if (t.type === TransformType.Scale) scale(ctx, t.value);
