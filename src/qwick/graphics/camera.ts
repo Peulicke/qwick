0c7f4c9c3ece2de8faf9ccf4a9cef27323d9f035
Oscar <oscarrpeulicke@gmail.com>
2025-05-10 13:53:08 +0200

    graphics context

--- src/qwick/graphics/camera.ts
+++ src/qwick/graphics/camera.ts
@@ -4,18 +4,23 @@ import type { Graphics } from ".";
 export type CameraState = {
     pos: vec2.Vec2;
     zoom: number;
+    verticalInversion: boolean;
 };
+
+export type Context = (graphics: Graphics, func: () => void) => void;
 
 export type Camera = {
     state: CameraState;
     screenToWorldCoords: (pos: vec2.Vec2) => vec2.Vec2;
     graphicsTransform: (graphics: Graphics) => void;
+    context: Context;
 };
 
 export const createCamera = (partialState: Partial<CameraState>): Camera => {
     const defaultState: CameraState = {
         pos: [0, 0],
-        zoom: 1
+        zoom: 1,
+        verticalInversion: false
     };
     const state = Object.assign(defaultState, partialState);
     const screenToWorldCoords: Camera["screenToWorldCoords"] = pos =>
@@ -24,9 +29,14 @@ export const createCamera = (partialState: Partial<CameraState>): Camera => {
         graphics.scale(state.zoom);
         graphics.translate(vec2.negate(state.pos));
     };
+    const context: Context = (graphics, func) => {
+        const c = state.verticalInversion ? graphics.contextVerticalInversion : graphics.context;
+        c(func);
+    };
     return {
         state,
         screenToWorldCoords,
-        graphicsTransform
+        graphicsTransform,
+        context
     };
 };
