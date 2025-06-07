ecdede2e9b16def50eb2591c0e01cc6ec3a067d2
Oscar <oscarrpeulicke@gmail.com>
2025-06-07 17:11:31 +0200

    flip y coordinate

--- src/qwick/graphics/camera3d.ts
+++ src/qwick/graphics/camera3d.ts
@@ -13,7 +13,7 @@ export type Camera3d = {
     graphicsTransform: (graphics: Graphics3d) => void;
 };
 
-const proj = (v: vec3.Vec3): vec2.Vec2 => [v[0], (v[1] - v[2]) * Math.SQRT1_2];
+const proj = (v: vec3.Vec3): vec2.Vec2 => [v[0], (v[2] - v[1]) * Math.SQRT1_2];
 
 export const createCamera3d = (partialState: Partial<Camera3dState>): Camera3d => {
     const defaultState: Camera3dState = {
@@ -24,7 +24,7 @@ export const createCamera3d = (partialState: Partial<Camera3dState>): Camera3d =
     const state = Object.assign(defaultState, partialState);
 
     const screenToWorldCoords: Camera3d["screenToWorldCoords"] = pos =>
-        vec3.add(vec3.scale([pos[0], 0, -pos[1] * Math.SQRT2], 1 / state.zoom), state.pos);
+        vec3.add(vec3.scale([pos[0], 0, pos[1] * Math.SQRT2], 1 / state.zoom), state.pos);
 
     const worldToScreenCoords: Camera3d["worldToScreenCoords"] = pos =>
         proj(vec3.scale(vec3.sub(pos, state.pos), state.zoom));
