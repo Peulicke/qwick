8b8e2fe1277f6a14593a687b85e36a78b5251d4e
Oscar <oscarrpeulicke@gmail.com>
2025-06-07 17:55:38 +0200

    camera orient

--- src/qwick/graphics/camera3d.spec.ts
+++ src/qwick/graphics/camera3d.spec.ts
@@ -1,9 +1,23 @@
-import type { vec2 } from "@peulicke/geometry";
+import { orient } from "@peulicke/geometry";
 import { createCamera3d } from "./camera3d";
 import { expect, it } from "vitest";
 
-it("converts screen to world pos and back again", () => {
-    const cam = createCamera3d({ pos: [1, 2, 3], zoom: 10 });
-    const pos: vec2.Vec2 = [0.1, 0.2];
-    expect(cam.worldToScreenCoords(cam.screenToWorldCoords(pos))).toBeCloseToVec2(pos);
+it("converts world to screen coords", () => {
+    const cam = createCamera3d({});
+    expect(cam.worldToScreenCoords([10, 20, 30])).toBeCloseToVec2([10, 20]);
+});
+
+it("converts world to screen coords for zoom", () => {
+    const cam = createCamera3d({ zoom: 10 });
+    expect(cam.worldToScreenCoords([10, 20, 30])).toBeCloseToVec2([100, 200]);
+});
+
+it("converts world to screen coords for pos", () => {
+    const cam = createCamera3d({ pos: [1, 2, 3] });
+    expect(cam.worldToScreenCoords([10, 20, 30])).toBeCloseToVec2([10 - 1, 20 - 2]);
+});
+
+it("converts world to screen coords for orient", () => {
+    const cam = createCamera3d({ orient: orient.fromAxisAngle([0, 1, 0], Math.PI / 2) });
+    expect(cam.worldToScreenCoords([10, 20, 30])).toBeCloseToVec2([30, 20]);
 });
--- src/qwick/graphics/camera3d.ts
+++ src/qwick/graphics/camera3d.ts
@@ -1,42 +1,40 @@
-import { vec2, vec3 } from "@peulicke/geometry";
+import { orient, vec2, vec3 } from "@peulicke/geometry";
 import type { Graphics3d } from ".";
 
 export type Camera3dState = {
     pos: vec3.Vec3;
     zoom: number;
+    orient: orient.Orient;
 };
 
 export type Camera3d = {
     state: Camera3dState;
-    screenToWorldCoords: (pos: vec2.Vec2) => vec3.Vec3;
     worldToScreenCoords: (pos: vec3.Vec3) => vec2.Vec2;
     graphicsTransform: (graphics: Graphics3d) => void;
 };
 
-const proj = (v: vec3.Vec3): vec2.Vec2 => [v[0], (v[2] - v[1]) * Math.SQRT1_2];
+const proj = (v: vec3.Vec3): vec2.Vec2 => [v[0], v[1]];
 
 export const createCamera3d = (partialState: Partial<Camera3dState>): Camera3d => {
     const defaultState: Camera3dState = {
         pos: [0, 0, 0],
-        zoom: 1
+        zoom: 1,
+        orient: orient.identity()
     };
 
     const state = Object.assign(defaultState, partialState);
 
-    const screenToWorldCoords: Camera3d["screenToWorldCoords"] = pos =>
-        vec3.add(vec3.scale([pos[0], 0, pos[1] * Math.SQRT2], 1 / state.zoom), state.pos);
-
     const worldToScreenCoords: Camera3d["worldToScreenCoords"] = pos =>
-        proj(vec3.scale(vec3.sub(pos, state.pos), state.zoom));
+        proj(orient.rotateVec3(state.orient, vec3.scale(vec3.sub(pos, state.pos), state.zoom)));
 
     const graphicsTransform: Camera3d["graphicsTransform"] = graphics => {
+        graphics.orient(state.orient);
         graphics.scale([state.zoom, state.zoom, state.zoom]);
         graphics.translate(vec3.negate(state.pos));
     };
 
     return {
         state,
-        screenToWorldCoords,
         worldToScreenCoords,
         graphicsTransform
     };
