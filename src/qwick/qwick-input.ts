e231fde18a322a44690b7af14de80467dc8fb4a2
Oscar <oscarrpeulicke@gmail.com>
2025-12-11 21:46:06 +0100

    fix

--- src/qwick/qwick-input.ts
+++ src/qwick/qwick-input.ts
@@ -2,7 +2,7 @@ import type { vec2 } from "@peulicke/geometry";
 import type { QwickInput } from ".";
 import type { Input, InputType } from "./input";
 
-const invertVertically = (pos: vec2.Vec2): vec2.Vec2 => [pos[0], window.innerHeight - pos[1]];
+const invertVertically = (pos: vec2.Vec2, height: number): vec2.Vec2 => [pos[0], height - pos[1]];
 
 export const createQwickInput = (input: Input): QwickInput => ({
     mousePos: {
@@ -11,9 +11,9 @@ export const createQwickInput = (input: Input): QwickInput => ({
         getPixels: () => input.mousePos
     },
     mousePosInverted: {
-        get: () => invertVertically(input.getMousePos()),
-        getPressed: () => invertVertically(input.getMousePressedPos()),
-        getPixels: () => invertVertically(input.mousePos)
+        get: () => invertVertically(input.getMousePos(), 1),
+        getPressed: () => invertVertically(input.getMousePressedPos(), 1),
+        getPixels: () => invertVertically(input.mousePos, window.innerHeight)
     },
     isKeyDown: (key: InputType) => input.keysDown.has(key),
     wasKeyPressed: (key: InputType) => input.keysPressed.has(key),
